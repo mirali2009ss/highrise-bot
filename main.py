d2bae2ddbdd53355caf0c7da326384aee689e9b4
mirali2009ss <amirbashang2009@gmail.com>
2026-06-04 13:25:10 +0330

    Add MyBot class with on_start method
    Implement a basic bot structure with an on_start method.

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+from highrise import BaseBot
+from highrise.__main__ import main
+
+class MyBot(BaseBot):
+    async def on_start(self, classroom_id: str, session_id: str) -> None:
+        print("ربات آنلاین شد!")
+
+if __name__ == "__main__":
+    main()
+  
