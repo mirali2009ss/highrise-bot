0d971e0d2e819c672be3b5362c76c69d0b23d1ed
mirali2009ss <amirbashang2009@gmail.com>
2026-06-04 19:46:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,29 +5,25 @@ from highrise import __main__ as highrise_main
 import sys
 from aiohttp import web
 
-# یک سرورِ بسیار ساده برای گول زدنِ رندر که پورتِ مورد نظرش را باز نگه دارد
-async def handle(request):
-    return web.Response(text="Bot is running!")
-
 class MyBot(BaseBot):
     async def on_start(self, session_metadata) -> None:
-        print("ربات با موفقیت در اتاق مستقر شد!")
+        print("ربات در اتاق مستقر شد!")
 
-async def start_server():
+async def web_server(request):
+    return web.Response(text="Bot is running")
+
+async def main():
+    # ۱. راه اندازی سرور برای جلوگیری از بسته شدن ربات توسط رندر
     app = web.Application()
-    app.router.add_get('/', handle)
+    app.router.add_get('/', web_server)
     runner = web.AppRunner(app)
     await runner.setup()
     site = web.TCPSite(runner, '0.0.0.0', int(os.environ.get("PORT", 8080)))
     await site.start()
 
-async def main():
-    # اجرای سرور وب برای جلوگیری از اخراج توسط رندر
-    await start_server()
-    
+    # ۲. اجرای خود ربات
     room_id = os.environ.get("ROOM_ID")
     token = os.environ.get("TOKEN")
-    
     sys.argv = ["highrise", "main:MyBot", room_id, token]
     highrise_main.main()
 
