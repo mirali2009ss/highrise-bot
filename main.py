6b5de767afba5080a6a0084e2c38d139b2e7700b
mirali2009ss <amirbashang2009@gmail.com>
2026-06-04 17:22:30 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import os
 import threading
 from http.server import SimpleHTTPRequestHandler, HTTPServer
-from highrise import BaseBot, Bot
+from highrise import BaseBot
 
-class MyBot(BaseBot):
-    async def on_start(self, session_metadata) -> None:
-        print("ربات با موفقیت آنلاین شد!")
-
+# سرور فیک برای رندر
 def run_fake_server():
     port = int(os.environ.get("PORT", 10000))
     server = HTTPServer(('0.0.0.0', port), SimpleHTTPRequestHandler)
     server.serve_forever()
 
+threading.Thread(target=run_fake_server, daemon=True).start()
+
+# کلاس ربات (فقط BaseBot)
+class MyBot(BaseBot):
+    async def on_start(self, session_metadata) -> None:
+        print("ربات با موفقیت آنلاین شد!")
+
 if __name__ == "__main__":
-    threading.Thread(target=run_fake_server, daemon=True).start()
+    from highrise.__main__ import main
+    import sys
     
+    # گرفتن مستقیم متغیرها
     room_id = os.environ.get("ROOM_ID")
     token = os.environ.get("TOKEN")
     
-    # اجرای استاندارد
-    bot = MyBot()
-    Bot().run(bot, room_id, token)
-    
+    # اجرای مستقیم
+    sys.argv = ["highrise", "main:MyBot", room_id, token]
+    main()
