017b35d01c5c6d2ba4c3ff1b95a56818230cfa9e
mirali2009ss <amirbashang2009@gmail.com>
2026-06-04 14:58:43 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from highrise import BaseBot
 from highrise.__main__ import main
 
 class MyBot(BaseBot):
-    async def on_start(self, classroom_id: str, session_id: str) -> None:
+    async def on_start(self, session_metadata: any, classroom_id: str, session_id: str) -> None:
+        
         print("ربات آنلاین شد!")
 
 if __name__ == "__main__":
